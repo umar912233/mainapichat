f5bb1d3eed6524cc23d631b1f75dd41e7eab33d7
Muhammad Umar <umar.rajpoot9122@gmail.com>
2021-09-01 00:22:21 -0700

    update 14

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
 const axios = require('axios');
