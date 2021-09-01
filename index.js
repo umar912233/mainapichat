5260278613010f2d09b241747e764a0a96f2c319
Muhammad Umar <umar.rajpoot9122@gmail.com>
2021-08-31 20:42:40 -0700

    update 6

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const http = require('http');
 const axios = require('axios');
+const https = require('https');
+var mysql = require('mysql');
 const port = 8080;
 var fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const dialogFLowHelper = require('./dialogflow.helper');
+
+var mysqlConnection = mysql.createConnection({
+  host:'167.99.148.122',
+  user:'newuser',
+  password:'password',
+  database:'autochat'
+});
+
+mysqlConnection.connect((err)=>{
+  if(!err)
+  console.log('DB connected');
+  else
+  console.log('Not Connecte');
+});
 
 app.post('/', async (req, res) => {
 
@@ -53,54 +69,85 @@ app.post('/webhook', (req, res) => {
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
+    mysqlConnection.query('SELECT * FROM aichat_data WHERE location_id="'+req.param('id')+'"',(err,rows,fields)=>{
+     
+          res.send({
+              data:rows,
+              status: true
+          });
+     
+    });
   
-  const tag = req.body.fulfillmentInfo.tag;
-  const session_name = req.body.sessionInfo.session;
-  
-  if(tag == 'aichat_contact'){
-  
-    var url = 'https://mybusiness.chat/api.php?location='+session_name;
+//   const tag = req.body.fulfillmentInfo.tag;
+//   const session_name = req.body.sessionInfo.session;
 
-    axios.get(url)
-  .then(response => {
-    console.log('this is response '+ response);
-    jsonResponse = {
-        fulfillment_response: {
-          messages: [
-            {
-              text: {
-                //fulfillment text response to be sent to the agent
-                text: [" - Hi! This is contact webhook response"]
-              }
-            }
-          ]
-        }
-      };
+// var url = 'https://mybusiness.chat/api.php?location=projects/wallet-campaigns-307112/locations/us-central1/agents/9e7f3111-ee1c-40fb-a144-47458e297c13/sessions/16';
+// axios.get(url)
+// .then(response => {
+// console.log('this is response '+ response);
+// jsonResponse = response;
+
+// res.json(jsonResponse);
+// // jsonResponse = {
+// //     fulfillment_response: {
+// //       messages: [
+// //         {
+// //           text: {
+// //             //fulfillment text response to be sent to the agent
+// //             text: [" - Hi! This is contact webhook response"]
+// //           }
+// //         }
+// //       ]
+// //     }
+// //   };
+  
+// })
+  
+//   if(tag == 'aichat_contact'){
+  
+//     var url = 'https://mybusiness.chat/api.php?location=projects/wallet-campaigns-307112/locations/us-central1/agents/9e7f3111-ee1c-40fb-a144-47458e297c13/sessions/16';
+
+//     axios.get(url)
+//   .then(response => {
+//     console.log('this is response '+ response);
+//     jsonResponse = response;
+//     // jsonResponse = {
+//     //     fulfillment_response: {
+//     //       messages: [
+//     //         {
+//     //           text: {
+//     //             //fulfillment text response to be sent to the agent
+//     //             text: [" - Hi! This is contact webhook response"]
+//     //           }
+//     //         }
+//     //       ]
+//     //     }
+//     //   };
       
-  })
-  .catch(error => {
-    console.log('this is error '+ error);
-  });
+//   })
+//   .catch(error => {
+//     console.log('this is error '+ error);
+//   });
 
   
     
-  }else if(tag == 'aichat_booking'){
+//   }else if(tag == 'aichat_booking'){
 
-    jsonResponse = {
-      fulfillment_response: {
-        messages: [
-          {
-            text: {
-              //fulfillment text response to be sent to the agent
-              text: [" - Hi! This is booking webhook response"]
-            }
-          }
-        ]
-      }
-    };
+//     jsonResponse = {
+//       fulfillment_response: {
+//         messages: [
+//           {
+//             text: {
+//               //fulfillment text response to be sent to the agent
+//               text: [" - Hi! This is booking webhook response"]
+//             }
+//           }
+//         ]
+//       }
+//     };
 
-  }
-  res.json(jsonResponse);
+//   }
+ 
 });
 
 app.listen(port, () => {
